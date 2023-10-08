4cc7f0be302fdfc4bbf5a8d7497ece945c75e50d
vinayakwho <115823570+vinayakwho@users.noreply.github.com>
2023-10-08 16:25:58 +0530

    Time: 20 ms (41.86%), Space: 19.5 MB (29.14%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=0){
+            swap(nums[k],nums[i]);
+            k++;
+            }
+        }
+        for(k; k<nums.size(); k++){
+            nums[k]=0;
+        }
+    }
+};
