3cd7531200e62a81f675af836f486714e8125ab1
vinayakwho <115823570+vinayakwho@users.noreply.github.com>
2023-10-20 12:37:05 +0530

    Time: 2 ms (50.75%), Space: 8.5 MB (19.27%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int>temp=heights;
-        sort(heights.begin(), heights.end());
-        int count=0;
-        for(int i=0; i<heights.size(); i++){
-            if(heights[i]!=temp[i]) count++;
-        
-        }
-        return count;
+       vector<int>vec;
+       int abc=0;
+       for(int i=0; i<heights.size(); i++){
+           vec.push_back(heights[i]);
+       }
+       sort(vec.begin(),vec.end());
+       for(int i=0; i<heights.size(); i++){
+           if(vec[i]!=heights[i]){
+               abc++;
+           }
+       }
+       return abc;
     }
 };
