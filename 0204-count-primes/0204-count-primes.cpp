60bb0d77fa141907bbe8f6065d3154b6a63c746f
vinayakwho <115823570+vinayakwho@users.noreply.github.com>
2024-06-08 15:55:50 +0530

    Time: 428 ms (50.66%), Space: 11.5 MB (43.66%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        int cnt = 0;
-        vector<bool> prime(n + 1, true);
+        if (n <= 2) return 0;
+        vector<bool> prime(n, true);
         prime[0] = prime[1] = false;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i * i < n; i++) {
             if (prime[i]) {
-                cnt++;
-                for (int j = 2 * i; j < n; j = j + i) {
-                    prime[j] = 0;
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
                 }
             }
+        }
+        int cnt = 0;
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) cnt++;
         }
         return cnt;
     }
