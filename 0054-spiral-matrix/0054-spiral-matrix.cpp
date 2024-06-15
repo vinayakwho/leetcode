807a3f64ddbe953859412f822340a26dcd16ad9d
vinayakwho <115823570+vinayakwho@users.noreply.github.com>
2024-06-16 01:29:20 +0530

    Time: 0 ms (100%), Space: 8.2 MB (52.33%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -21,14 +21,14 @@ public:
         while(count < total){
             
             // Print starting row
-            for(int index = startingCol; index <= endingCol; index++){
+            for(int index = startingCol; index <= endingCol && count < total; index++){
                 ans.push_back(matrix[startingRow][index]);
                 count++;
             }
             startingRow++;
 
             // Print ending column
-            for(int index = startingRow; index <= endingRow; index++){
+            for(int index = startingRow; index <= endingRow && count < total; index++){
                 ans.push_back(matrix[index][endingCol]);
                 count++;
             }
